3469398e33c53c0785fd56782df3d764b4ae961c
sunnylqm <sunnylqm@qq.com>
2020-01-30 23:52:31 +0800

    update api endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,9 +4,9 @@ import { Dimensions } from 'react-native';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 export { screenHeight, screenWidth };
-// export const isIphoneX = DeviceInfo.isIPhoneX_deprecated;
-export const host = __DEV__ ? 'http://localhost:7001' : 'http://118.24.22.67';
-// export const host = 'http://118.24.22.67';
+
+// export const host = __DEV__ ? 'http://localhost:7001' : 'http://139.9.155.96';
+export const host = 'http://139.9.155.96';
 export const apiEndpoint = `${host}/api`;
 
 export const getImageUrl = (imageId: number) => `${apiEndpoint}/file/${imageId}`;
