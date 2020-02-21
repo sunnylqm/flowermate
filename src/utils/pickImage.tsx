3265fe322fd8b4e5530987d23af428835e72f86b
sunnylqm <sunnylqm@qq.com>
2020-02-21 18:13:00 +0800

    Fix modal cancel

--- src/utils/pickImage.tsx
+++ src/utils/pickImage.tsx
@@ -1,20 +1,18 @@
 import ActionSheet from 'react-native-action-sheet';
-import ImagePicker, { Image as ImageFile } from 'react-native-image-crop-picker';
+import ImagePicker, {
+  Image as ImageFile,
+} from 'react-native-image-crop-picker';
 
-export function openPicker() {
+export async function openPicker() {
   return ImagePicker.openPicker({
-    // compressImageMaxWidth: 1200,
-    // compressImageMaxHeight: 1200,
     compressImageMaxWidth: 600,
     compressImageMaxHeight: 600,
     forceJpg: true,
   }).catch(e => {});
 }
 
-export function openCamera() {
+export async function openCamera() {
   return ImagePicker.openCamera({
-    // width: 900,
-    // height: 1200,
     width: 450,
     height: 600,
     cropping: true,
@@ -26,7 +24,8 @@ export default function pickImage(): Promise<ImageFile | null> {
   return new Promise(resolve => {
     ActionSheet.showActionSheetWithOptions(
       {
-        options: ['拍摄', '从相册选择'],
+        options: ['拍摄', '从相册选择', '取消'],
+        cancelButtonIndex: 2,
       },
       buttonIndex => {
         if (buttonIndex === 0) {
