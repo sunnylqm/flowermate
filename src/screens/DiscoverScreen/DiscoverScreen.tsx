fca0f709320c87aae05523cd72f072dc8de3054a
sunnylqm <sunnylqm@gmail.com>
2021-03-18 00:50:03 +0800

    Add discoverscreen

--- src/screens/DiscoverScreen/DiscoverScreen.tsx
+++ src/screens/DiscoverScreen/DiscoverScreen.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import {
+  FlatList,
+  Image,
+  ImageSourcePropType,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+interface Item {
+  name: string;
+  cover: ImageSourcePropType;
+  price: number;
+}
+const data: Item[] = [
+  { name: 'Brass Planter', cover: require('assets/plants/1.jpg'), price: 198 },
+  { name: 'Fox Planter', cover: require('assets/plants/2.jpg'), price: 34 },
+];
 function DiscoverScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <TabView
-        navigationState={state}
-        renderScene={sceneMap}
-        onIndexChange={index => setState({ ...state, index })}
-        initialLayout={{ width: screenWidth }}
-      /> */}
+      <View style={styles.header}>
+        <Icon style={styles.headerLeft} name="bars" size={24} />
+        <Text style={styles.headerTitle}>商城</Text>
+        <Icon style={styles.headerRight} name="shopping-bag" size={24} />
+      </View>
+      <FlatList<Item>
+        style={styles.list}
+        data={data}
+        numColumns={2}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => <Card item={item} />}
+      />
     </SafeAreaView>
+  );
+}
+function Card({ item }: { item: Item }) {
+  return (
+    <View style={styles.card}>
+      <Image style={styles.cover} source={item.cover} />
+      <Text style={styles.title}>{item.name}</Text>
+      <Text style={styles.price}>￥{item.price.toFixed(2)}</Text>
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -18,21 +51,48 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#efefef',
   },
-  // listRow: {
-  //   overflow: 'hidden',
-  //   margin: 15,
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-between',
-  //   alignItems: 'center',
-  // },
-  // avatar: {
-  //   width: 48,
-  //   height: 48,
-  //   borderRadius: 24,
-  //   marginRight: 10,
-  //   resizeMode: 'cover',
-  // },
-  // order: {},
-  // username: {},
+  header: {
+    padding: 18,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: 'grey',
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  headerLeft: {
+    padding: 10,
+    position: 'absolute',
+    left: '6%',
+  },
+  headerRight: {
+    padding: 10,
+    position: 'absolute',
+    right: '6%',
+  },
+  card: {
+    flex: 1,
+    padding: 10,
+  },
+  cover: {
+    width: '100%',
+    resizeMode: 'cover',
+    height: 220,
+    borderRadius: 6,
+  },
+  title: {
+    paddingTop: 12,
+    paddingBottom: 5,
+    paddingLeft: 2,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  price: {},
+  list: {
+    margin: 15,
+  },
 });
 export default DiscoverScreen;
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#efefef',
   },
   header: {
-    marginTop: 40,
+    padding: 18,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
